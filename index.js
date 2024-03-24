8454282583de70a3bbb689c265c2ba5ba80901ef
niyatiashar17 <ashar.n@northeatsern.edu>
2024-03-24 16:24:38 -0400

    consolelog

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const functions = require("@google-cloud/functions-framework");
 
 functions.cloudEvent("serverless_verify_email", (cloudEvent) => {
   const base64name = cloudEvent.data.message.data;
+  console.log(cloudEvent);
 
   const message = base64name
     ? JSON.parse(Buffer.from(base64Message, "base64").toString())
