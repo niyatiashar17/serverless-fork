fe1248243b3c6dda86476ec6021f3fa85c70e3a6
niyatiashar17 <ashar.n@northeatsern.edu>
2024-03-25 11:05:57 -0400

    bufferchanges

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const functions = require("@google-cloud/functions-framework");
+const mailgun = require("mailgun-js");
 
 functions.cloudEvent("serverless_verify_email", (cloudEvent) => {
-  const base64name = cloudEvent.data.message.data;
+  const base64name = JSON.parse(
+    Buffer.from(cloudEvent.data.message.data, "base64").toString()
+  );
   console.log(cloudEvent);
 
   const message = base64name
     ? JSON.parse(Buffer.from(base64Message, "base64").toString())
     : {};
-
-  console.log(`Email, ${email}!`);
 });
